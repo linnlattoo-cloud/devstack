c82352824b8b4a9ed2a077a9d5aff239bd0c7059
Linn Latt Oo <132224170+linnlattoo-cloud@users.noreply.github.com>
2025-05-25 23:45:29 +0700

    Update devstack_setup.sh

--- devstack_setup.sh
+++ devstack_setup.sh
@@ -1,13 +1,11 @@
-#! /bin/sh
+#!/bin/bash
 
-export LANG=en_US.utf-8
-export LC_ALL=en_US.utf-8
-
-sudo apt-get install -y git
+sudo apt-get install -y git build-essential module-assistant
 
 git clone https://git.openstack.org/openstack-dev/devstack
 
 cp /devstack/local.conf devstack
 
 cd devstack
+
 ./stack.sh
